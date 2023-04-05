dfe36d3aea2149960b21d292fe3069beddca8a9d
AleksandrBashkaev <Bashkaev_a@mail.ru>
2023-04-05 21:38:32 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,24 @@ public class Main {
     public static void main(String[] args) {
 
 
-    int startMoney = 300;//Деньги на счету
-    int addMoney = 1000;//Сумма пополнения
-    int bonus; //Количество бонусов
-    int finalMoney; //Итог
+        int startMoney = 300;//Деньги на счету
+        int addMoney = 1000;//Сумма пополнения
+        int bonus; //Количество бонусов
+        int finalMoney; //Итог
 
-    if ( addMoney >= 1000 ) {
-        bonus = addMoney / 100;
-    } else {
-        bonus = 0;
-    }
+        if (addMoney >= 1000) {
+            bonus = addMoney / 100;
+        } else {
+            bonus = 0;
+        }
 
- finalMoney = startMoney + addMoney + bonus;
+        finalMoney = startMoney + addMoney + bonus;
 
-      if (bonus == 0) {
-          System.out.println( "Баланс пополнен на " + addMoney + ". Сумма на счету составляет " + finalMoney);
-      } else {
-          System.out.println( "Баланс пополнен на " + addMoney + " руб. Получено бонусов " + bonus + " руб. Сумма на счету составляет " + finalMoney +  " руб.");
-      }
-
+        if (bonus == 0) {
+            System.out.println("Баланс пополнен на " + addMoney + ". Сумма на счету составляет " + finalMoney);
+        } else {
+            System.out.println("Баланс пополнен на " + addMoney + " руб. Получено бонусов " + bonus + " руб. Сумма на счету составляет " + finalMoney + " руб.");
+        }
 
 
     }
